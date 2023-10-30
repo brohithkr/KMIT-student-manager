6519b60187ababfeff62c29434de16f41948ac8c
brohithkr <brohithkr@gmail.com>
2023-10-30 12:42:33 +0530

    complete issued passes

--- server/passes/views.py
+++ server/passes/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse, HttpRequest
 from ninja import NinjaAPI
 from ninja.security import HttpBearer
 from passes.models import *
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import relativedelta
+
+import passes.secret_config as secrets
+
 class Auth(HttpBearer):
     def authenticate(self, request, token):
-        if token == "hello":
+        if token == secrets.auth_token:
             return True
         return False
 
@@ -15,30 +18,31 @@ api = NinjaAPI(auth=Auth())
 
 @api.post("/gen_pass")
 def gen_pass(request: HttpRequest, reqPass: ReqPass ):
-
     today = datetime.now()
 
     already_issued = IssuedPass.objects.filter(
         roll_no = reqPass.roll_no,
-        valid_till__greater_than = int(today.timestamp())
+        valid_till__gt= int(today.timestamp())
     )
 
-    if already_issued.count > 0:
+    if already_issued.count() > 0:
         return HttpResponse(f"Warning: {reqPass.roll_no} already owns a pass")
-    
+
+    valid_till = today
     if reqPass.pass_type == "one_time" :
-        valid_till = today.timestamp + (24*60*60)
+        valid_till = today + timedelta(days=1)
     if reqPass.pass_type == "daily" :
         valid_till = today + relativedelta(months=6)
     if reqPass.pass_type == "alumni":
         valid_till = today + relativedelta(years=70)
     
-    # IssuedPass.objects.create(
-    #     roll_no = 
-    # )
+    
+    IssuedPass.objects.create(
+        roll_no=reqPass.roll_no,
+        pass_type=reqPass.pass_type,
+        issues_date=today.timestamp(),
+        valid_till=valid_till.timestamp()
+    )
+    return "success"
 
-
-
-
-    pass
 
