0c7b51f89d1b153d3c0e3a9a3cd204dd2839c53c
brohithkr <brohithkr@gmail.com>
2023-11-04 18:47:49 +0530

    other mirations

--- server/server/views.py
+++ server/server/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from ninja import NinjaAPI
+from passes.models import Student, init_students
 
-
-def home(request: HttpRequest):
+def home(request: HttpRequest, initdb=False):
     print(request.method)
+    init_students()
     return HttpResponse("hello world")
