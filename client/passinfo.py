61463d50ce6fad9a82ad315db340a8a01608ed0f
Jai-JAP <jai.jap.318@gmail.com>
2024-03-17 18:38:37 +0530

    Show details in ui

--- client/passinfo.py
+++ client/passinfo.py
@@ -1,8 +1,17 @@
-from PyQt5.QtWidgets import QDialog, QLabel, QFormLayout, QDialogButtonBox
+from PyQt5.QtWidgets import (
+    QDialog,
+    QLabel,
+    QLayout,
+    QVBoxLayout,
+    QFormLayout,
+    QDialogButtonBox,
+    QPushButton,
+)
 from PyQt5.QtGui import QCloseEvent
 from PyQt5.QtCore import Qt
 
 from requests import post as urlpost
+# from datetime import datetime
 
 from srvrcfg import SERVERURL, headers, TIMEOUT
 
@@ -28,10 +37,25 @@ class PassInfoDialog(QDialog):
             case "namaaz":
                 self.details["pass_type"] = "Namaaz"
 
+        # self.details["issued_on"] = datetime.fromtimestamp(int(self.details["issued_on"])).strftime("%d-%m-%Y, %I:%M %p") # utc epoch to string
+
         self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok, self)
-        buttonBox.addButton("Revoke", QDialogButtonBox.ButtonRole.ActionRole)
 
+        revokeButton = QPushButton("Revoke", buttonBox)
+        buttonBox.addButton(revokeButton, QDialogButtonBox.ButtonRole.DestructiveRole)
+
+        form = QFormLayout()
+        form.addRow("Pass Type", QLabel(self.details["pass_type"]))
+        form.addRow("Issued On", QLabel(self.details["issued_on"]))
+
+        layout = QVBoxLayout(self)
+        layout.addLayout(QFormLayout)
+        layout.addWidget(buttonBox)
+
+        self.layout().setSizeConstraint(QLayout.SetFixedSize)
+
+        revokeButton.clicked.connect(self.revokePass)
         buttonBox.accepted.connect(self.close)
 
     def revokePass(self):
@@ -42,7 +66,7 @@ class PassInfoDialog(QDialog):
                 json={"roll_no": self.details["rno"]},
                 timeout=TIMEOUT,
             )
-        except (ConnectionError, TimeoutError): 
+        except (ConnectionError, TimeoutError):
             self.parent().error("Connection Error!\nCheck Connection & Try again.")
             self.parent().status.setText("Connection Error.")
             self.error = True
